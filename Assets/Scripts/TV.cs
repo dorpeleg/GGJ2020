35c92f1b08dfe53987db3ec0410af35681995068
Dor Peleg <dorpelegg@gmail.com>
2020-02-01 10:54:15 +0200

    infinte levels

--- Assets/Scripts/TV.cs
+++ Assets/Scripts/TV.cs
@@ -6,7 +6,6 @@ using UnityEngine.Video;
 public class TV : MonoBehaviour
 {
     [SerializeField] private TVScenes _tvScenes;
-    [SerializeField] private int _currentScene = 0;
     [SerializeField] private float _hitGoodThreshold = 3f;
     [SerializeField] private float _hitLoseThreshold = 3f;
     [SerializeField] private float _minTimeBetweenScenes = 1f;
@@ -17,14 +16,13 @@ public class TV : MonoBehaviour
     [SerializeField] private VideoPlayer _videoPlayer;
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private MeshRenderer _snowMeshRenderer;
+
+    private int _currentScene = 0;
     private bool _calledNow = false;
     private bool _gameOver = false;
     private float _nextFire = 0f;
     private SwipeDirection _neededDirection;
     private AudioSource _audio;
-
-    // TEMP
-    public UnityEngine.UI.Text Label;
 
     private const string ShaderKeyword = "_Direction";
 
@@ -56,7 +54,6 @@ public class TV : MonoBehaviour
                 int directionNumber = Random.Range(2, 5);
                 _neededDirection = (SwipeDirection)directionNumber;
                 Debug.Log("NOW! " + _neededDirection.ToString());
-                Label.text = _neededDirection.ToString();
                 _snowMeshRenderer.material.SetFloat(ShaderKeyword, directionNumber);
                 _calledNow = true;
             }
@@ -98,42 +95,36 @@ public class TV : MonoBehaviour
     {
         if(_neededDirection == direction)
         {
-            if (_currentScene < _tvScenes.Scenes.Count)
+            if (Time.time <= _nextFire + _hitGoodThreshold)
             {
-                if (Time.time <= _nextFire + _hitGoodThreshold)
-                {
-                    Debug.Log("current scene is " + _tvScenes.Scenes[_currentScene]);
-                }
-                else
-                {
-                    Debug.Log("It's too late! current scene is " + _tvScenes.Scenes[_currentScene]);
-                }
-                PlayNextTVScene();
-                _timeBetweenScenes = Random.Range(_minTimeBetweenScenes, _maxTimeBetweenScenes);
-                _nextFire = Time.time + _timeBetweenScenes;
-                _calledNow = false;
-                Debug.Log("NEW ROUND");
-                Label.text = "";
-                _snowMeshRenderer.material.SetFloat(ShaderKeyword, 0);
+                Debug.Log("current scene is " + _tvScenes.Scenes[_currentScene]);
             }
             else
             {
-                Debug.Log("You Won!");
-                Label.text = "";
-                _gameOver = true;
+                Debug.Log("It's too late! current scene is " + _tvScenes.Scenes[_currentScene]);
             }
+            PlayNextTVScene();
+            _timeBetweenScenes = Random.Range(_minTimeBetweenScenes, _maxTimeBetweenScenes);
+            _nextFire = Time.time + _timeBetweenScenes;
+            _calledNow = false;
+            Debug.Log("NEW ROUND");
+            _snowMeshRenderer.material.SetFloat(ShaderKeyword, 0);
         }
         else
         {
             GameOver();
             Debug.Log("wrong move! " + direction.ToString());
-            Label.text = "";
         }
     }
 
     private void PlayNextTVScene()
     {
-        _currentScene++;
+        var newScene = Random.Range(0, _tvScenes.Scenes.Count - 1);
+        while(newScene == _currentScene)
+        {
+            newScene = Random.Range(0, _tvScenes.Scenes.Count - 1);
+        }
+        _currentScene = newScene;
         _videoPlayer.clip = _tvScenes.Scenes[_currentScene].VideoFile;
         _audioSource.Stop();
         _audioSource.PlayOneShot(_tvScenes.Scenes[_currentScene].AudioFile);
@@ -188,5 +179,4 @@ public class TV : MonoBehaviour
         yield return new WaitForSeconds(0.45f);
         kafaGraphic.SetActive(false);
     }
-
 }
